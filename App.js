02dc58ef28ccd36b999b25503dd9636b46732c53
Rommel Rico <me@rommelrico.com>
2019-08-15 06:43:14 -0700

    :sparkles: App: Aligning button to right of TextInput

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}} >
-      <View>
+      <View style={{flexDirection: "row", justifyContent: "space-between"}}>
         <TextInput placeholder="Course Goal" style={{borderColor: "black", borderWidth: 1, padding: 10}} />
         <Button title="ADD" />
       </View>
