0cc931dd5d6af155e7c183379e84821bb9daa6f8
Rommel Rico <me@rommelrico.com>
2019-08-19 21:03:50 -0700

    :sparkles: GoalInput: Adding some width to the buttons

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -24,8 +24,12 @@ const GoalInput = props => {
           value={enteredGoal}
         />
         <View style={styles.buttonContainer}>
-          <Button title="CANCEL" color="red" onPress={() => props.onCancel()} />
-          <Button title="ADD" onPress={addGoalHandler} />
+          <View style={styles.button}>
+            <Button title="CANCEL" color="red" onPress={() => props.onCancel()} />
+          </View>
+          <View style={styles.button}>
+            <Button title="ADD" onPress={addGoalHandler} />
+          </View>
         </View>
       </View>
     </Modal>
@@ -49,6 +53,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: "60%"
+  },
+  button: {
+    width: "40%"
   }
 });
 
